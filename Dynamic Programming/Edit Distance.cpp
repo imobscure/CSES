6a7f3d9a4fbef0137dd16e3d6e0ec1a06d6ace05
Prakhar Pratap Singh <77916096+imobscure@users.noreply.github.com>
2023-05-14 18:36:43 +0530

    Update Edit Distance.cpp

--- Dynamic Programming/Edit Distance.cpp
+++ Dynamic Programming/Edit Distance.cpp
@@ -1,36 +1,40 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
-using namespace std;
+using namespace std ;
+using int64 = long long ;
+const int64 mod = 1e9+7 ;
 
-int main(){
+void solve()
+{
+    string A, B ;
+    cin >> A >> B ;
 
-	string a,b;
-	cin>>a>>b;
+    int n = A.size(), m = B.size() ;
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 1e9)) ;
 
-	int n=a.size(),m=b.size();
+    dp[0][0] = 0 ;
+    for(int a = 1; a <= n; a++)
+        dp[a][0] = dp[a-1][0]+1 ;
+    for(int b = 1; b <= m; b++)
+        dp[0][b] = dp[0][b-1]+1 ;
 
-	vector<vector<int>> dp(n+1,vector<int>(m+1,1e9+7));
-	dp[0][0]=0;
+    for(int a = 1; a <= n; a++)
+        for(int b = 1; b <= m; b++)
+            dp[a][b] = min((int)(A[a-1]!= B[b-1])+dp[a-1][b-1], 1+min(dp[a-1][b], dp[a][b-1])) ;
 
-	for(int i=0;i<=n;i++){
-		for(int j=0;j<=m;j++){
+    cout << dp[n][m] ;
+}
 
-			if(i){
-				dp[i][j]=min(dp[i][j],dp[i-1][j]+1);
-			}
-			if(j){
-				dp[i][j]=min(dp[i][j],dp[i][j-1]+1);
-			}
-			if(i && j){
-				dp[i][j]=min(dp[i][j],dp[i-1][j-1]+(a[i-1]!=b[j-1]));
-			}
-
-		}
-	}
-
-	cout<<dp[n][m];
-
-	return 0;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1 ;
+    // cin >> test ;
+    for(int cas = 1; cas <= test; cas++)
+    {
+        solve() ;
+        cout << '\n' ;
+    }
+    return 0 ;
 }
