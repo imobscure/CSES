c440b8c0ba1c42b655f377700b436834cc8c177f
Prakhar Pratap Singh <77916096+imobscure@users.noreply.github.com>
2023-05-14 18:56:14 +0530

    Update Rectangle Cutting.cpp

--- Dynamic Programming/Rectangle Cutting.cpp
+++ Dynamic Programming/Rectangle Cutting.cpp
@@ -1,57 +1,45 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+using namespace std ;
+using int64 = long long ;
+const int64 mod = 1e9+7 ;
 
-#define int long long
+void solve()
+{
+    int a, b ;
+    cin >> a >> b ;
 
-using vi = vector<int>;
-#define pb push_back
-#define all(x) begin(x), end(x)
-#define sz(x) (int) (x).size()
+    vector<vector<int>> dp(a+1, vector<int>(b+1, 1e9)) ;
+    dp[0][0] = 0 ;
 
-using pi = pair<int,int>;
-#define f first
-#define s second
-#define mp make_pair
-
-#define re(i,n) for(int i=0;i<n;i++)
-#define reb(i,n) for(int i=n-1;i>=0;i--)
-#define test int t;cin>>t;while(t--)
-
-
-void setIO(string name = "") {
-	cin.tie(0)->sync_with_stdio(0);
-	if (sz(name)) {
-		freopen((name + ".in").c_str(), "r", stdin);
-		freopen((name + ".out").c_str(), "w", stdout);
-	}
+    for(int aa = 1; aa <= a; aa++)
+    {
+        for(int bb = 1; bb <= b; bb++)
+        {
+            if(aa == bb)
+                dp[aa][bb] = 0 ;
+            else
+            {
+                for(int aaa = 1; aaa < aa; aaa++)
+                    dp[aa][bb] = min(dp[aa][bb], dp[aaa][bb]+dp[aa-aaa][bb]+1) ;
+                for(int bbb = 1; bbb < bb; bbb++)
+                    dp[aa][bb] = min(dp[aa][bb], dp[aa][bbb]+dp[aa][bb-bbb]+1) ;
+            }
+        }
+    }
+    cout << dp[a][b] ;
 }
 
-signed main(){
-	setIO();
-
-	int a,b; cin>>a>>b;
-	vector<vector<int>> dp(501,vector<int>(501,-1));
-
-	auto solve = [&](auto&& solve,int x,int y)->int{
-
-		if(x<=0 || y<=0)return LLONG_MAX;
-		if(x==y)return 0ll;
-
-		if(dp[x][y]!=-1)return dp[x][y];
-
-		int find = LLONG_MAX;
-
-		for(int i=1;i<x;i++){
-			find = min(find,solve(solve,x-i,y)+solve(solve,i,y)+1ll);
-		}
-		for(int i=1;i<y;i++){
-			find = min(find,solve(solve,x,y-i)+solve(solve,x,i)+1ll);
-		}
-
-		return dp[x][y]=find;
-	};
-
-	cout<<solve(solve,a,b);
-
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1 ;
+    // cin >> test ;
+    for(int cas = 1; cas <= test; cas++)
+    {
+        solve() ;
+        cout << '\n' ;
+    }
+    return 0 ;
 }
